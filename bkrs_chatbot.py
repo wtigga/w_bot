916599c19d2610136c1689ac6b561075fbd604ff
wtigga <wtigga@gmail.com>
2018-11-18 18:25:13 +0800

    more answers and triggers

--- bkrs_chatbot.py
+++ bkrs_chatbot.py
@@ -5,8 +5,22 @@ from telebot.types import Message
 TOKEN = '794930488:AAEtpnGGR0NcidyV1JcA-tJOL0PjkI7DpiU'
 bot = telebot.TeleBot(TOKEN)
 
-trigger = ['обожаю китай', 'ненавижу китай', 'долбаный китай', 'сраный китай', 'китай...']
-answers = ['Ненавижу Китай!', 'Обожаю Китай!']
+trigger = ['обожаю китай',
+           'ненавижу китай',
+           'долбаный китай',
+           'сраный китай',
+           'китай...',
+           'любишь китай',
+           'хочешь в китай',
+           'любит китай']
+
+answers = ['Ненавижу Китай!',
+           'Обожаю Китай!',
+           'Великий Китай!',
+           'Пять тысяч лет истории!',
+           'Восемь тысяч лет истории!'
+           'Стопятьдесят тысяч лет истории!']
+
 
 @bot.message_handler(func=lambda message: True)
 def upper(message: Message):
@@ -23,5 +37,6 @@ def upper(message: Message):
         bot.reply_to(message, random.choice(answers))
     else:
         pass
+
 
 bot.polling()
