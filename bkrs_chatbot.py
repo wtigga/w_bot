e4764b2cb102996b114a77f63136ec6317095c88
wtigga <wtigga@gmail.com>
2018-11-23 16:03:06 +0800

    return old clean list logic

--- bkrs_chatbot.py
+++ bkrs_chatbot.py
@@ -9,28 +9,30 @@ with open('triggers.csv', 'r', encoding='utf-8') as file:  # read content from C
   read_list = list(reader)
 
 
-# def clean_list(my_list):  # clean CSV list of empty cells
-#     for i in my_list:
-#             try:
-#                 my_list.pop(my_list.index(''))
-#             except:
-#                 pass
-#     return my_list
+def clean_list(my_list):  # clean CSV list of empty cells
+     for i in my_list:
+             try:
+                 my_list.pop(my_list.index(''))
+             except:
+                 pass
+     return my_list
 
 
 TOKEN = os.environ.get('TOKEN')
 bot = telebot.TeleBot(TOKEN)
 
-# trigger_china = clean_list(read_list[0])
-# trigger_shenzhen = clean_list(read_list[1])
-# answers_china = clean_list(read_list[2])
-# answers_shenzhen = clean_list(read_list[3])
+trigger_china = clean_list(read_list[0])
+trigger_shenzhen = clean_list(read_list[1])
+answers_china = clean_list(read_list[2])
+answers_shenzhen = clean_list(read_list[3])
 
+'''
 #list comprehension
 trigger_china = set([i for i in read_list[0] if len(i) > 0])
 trigger_shenzhen = set([i for i in read_list[1] if len(i) > 0])
 answers_china = set([i for i in read_list[2] if len(i) > 0])
 answers_shenzhen = set([i for i in read_list[3] if len(i) > 0])
+'''
 
 
 @bot.message_handler(func=lambda message: True)
